38305764710d7e83cd786510ae5fbd1c91950c8f
sinem taşpınar <taspinarsinem@hotmail.com>
2024-03-22 01:31:01 +0300

    Add project files.

---
+++ FindYourZodiacSign/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace FindYourZodiacSign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblEntry = new System.Windows.Forms.Label();
+            this.lblPrint = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.tbBday = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Goudy Old Style", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnExit.Location = new System.Drawing.Point(453, 381);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(97, 28);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseCompatibleTextRendering = true;
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click_1);
+            // 
+            // lblEntry
+            // 
+            this.lblEntry.AutoSize = true;
+            this.lblEntry.Font = new System.Drawing.Font("Microsoft Tai Le", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEntry.Location = new System.Drawing.Point(147, 34);
+            this.lblEntry.Name = "lblEntry";
+            this.lblEntry.Size = new System.Drawing.Size(260, 22);
+            this.lblEntry.TabIndex = 1;
+            this.lblEntry.Text = "Please Enter Your Birthday";
+            this.lblEntry.Click += new System.EventHandler(this.lblEntry_Click);
+            // 
+            // lblPrint
+            // 
+            this.lblPrint.AutoSize = true;
+            this.lblPrint.Location = new System.Drawing.Point(159, 215);
+            this.lblPrint.Name = "lblPrint";
+            this.lblPrint.Size = new System.Drawing.Size(28, 13);
+            this.lblPrint.TabIndex = 2;
+            this.lblPrint.Text = "Print";
+            this.lblPrint.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOK.Location = new System.Drawing.Point(225, 123);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(92, 33);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "Okay";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // tbBday
+            // 
+            this.tbBday.Location = new System.Drawing.Point(162, 75);
+            this.tbBday.Name = "tbBday";
+            this.tbBday.Size = new System.Drawing.Size(233, 20);
+            this.tbBday.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 421);
+            this.ControlBox = false;
+            this.Controls.Add(this.tbBday);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.lblPrint);
+            this.Controls.Add(this.lblEntry);
+            this.Controls.Add(this.btnExit);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblEntry;
+        private System.Windows.Forms.Label lblPrint;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox tbBday;
+    }
+}
+
---
+++ FindYourZodiacSign/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindYourZodiacSign
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void lblEntry_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            TextBox bday= new TextBox();
+
+            bday = tbBday.Text; // textbox dan seçtiğim
+
+            int dDay = bday.Text; // Günü öğrenmek için
+            string dMonth= bday.Text; // Ayı öğrenmek için
+
+            string Sign = ZodiacSign("mounth", dDay);
+
+            MessageBox.Show($"According to your information, your Zodiac Sign : {Sign} ");
+        }
+
+        public static string ZodiacSign(string mounth, int day)
+
+        {
+
+            if ((mounth.ToLower() == "Mart" && day >= 21) || (mounth.ToLower() == "Nisan" && day <= 20))
+                return "Aries";
+            else if ((mounth.ToLower() == "Nisan" && day >= 21) || (mounth.ToLower() == "Mayıs" && day <= 21))
+                return "Taurus";
+            else if ((mounth.ToLower() == "Mayıs" && day >= 22) || (mounth.ToLower() == "Haziran" && day <= 21))
+                return "Gemini";
+            else if ((mounth.ToLower() == "Haziran" && day >= 22) || (mounth.ToLower() == "Temmuz" && day <= 22))
+                return "Cancer";
+            else if ((mounth.ToLower() == "Temmuz" && day >= 23) || (mounth.ToLower() == "Ağustos" && day <= 22))
+                return "Leo";
+            else if ((mounth.ToLower() == "Ağustos" && day >= 23) || (mounth.ToLower() == "Eylül" && day <= 22))
+                return "Virgo";
+            else if ((mounth.ToLower() == "Eylül" && day >= 23) || (mounth.ToLower() == "Ekim" && day <= 22))
+                return "Libra";
+            else if ((mounth.ToLower() == "Ekim" && day >= 23) || (mounth.ToLower() == "Kasım" && day <= 21))
+                return "Scorpio";
+            else if ((mounth.ToLower() == "Kasım" && day >= 22) || (mounth.ToLower() == "Aralık" && day <= 21))
+                return "Sagittarius";
+            else if ((mounth.ToLower() == "Aralık" && day >= 22) || (mounth.ToLower() == "Ocak" && day <= 20))
+                return "Capricorn";
+            else if ((mounth.ToLower() == "Ocak" && day >= 21) || (mounth.ToLower() == "Şubat" && day <= 18))
+                return "Aquarius";
+            else if ((mounth.ToLower() == "Şubat" && day >= 19) || (mounth.ToLower() == "Mart" && day <= 20))
+                return "Pisces";
+            else
+                return "Geçersiz Tarih girdiniz...";
+
+
+        }
+    }
+}
